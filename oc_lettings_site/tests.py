c429b253d58cea97cbfd2e9d5ad8dc24990f4eb0
erikia <nobileisabelle.in@gmail.com>
2023-01-24 18:11:55 +0100

    fix linting/flake8

--- oc_lettings_site/tests.py
+++ oc_lettings_site/tests.py
@@ -2,10 +2,11 @@ from django.urls import reverse
 from django.test import RequestFactory, TestCase
 from .views import index
 
+
 class IndexViewTestCase(TestCase):
     def setUp(self):
         self.factory = RequestFactory()
-    
+
     def test_index(self):
         url = reverse("index")
         request = self.factory.get(url)
--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -4,8 +4,6 @@ from django.urls import path, include
 from . import views
 
 
-
-
 urlpatterns = [
     path('', views.index, name='index'),
     path('lettings/', include(('lettings.urls', 'lettings'), namespace='lettings')),
