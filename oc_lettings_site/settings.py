13e710972a63151117757b1827f2f63b64f5575a
erikia <nobileisabelle.in@gmail.com>
2023-02-02 15:53:39 +0100

    test soutenance

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -2,6 +2,7 @@ import os
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
